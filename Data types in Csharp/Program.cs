02bef7d20a7673d6c4ee45b9a786cd0401c8b379
KATAMPOI <KATAMPOI@EUCROLA>
2024-09-02 19:43:21 +0300

    Add project files.

---
+++ Data types in Csharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = " Rhino Academy ";
+            char grade = 'A';
+            int age = 20;
+            double gpa = 3.0;
+            bool isMale = false;
+
+            Console.WriteLine("the academy is known as " +phrase+ " A grade of  " +grade+ " is guaranted at an age of " +age);
+
+
+            Console.ReadLine(); 
+        }
+    }
+}
